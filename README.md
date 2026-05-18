987b2282b7c87d305fc6e0b2f717f0cddd323d22
Vinayak G Bhat <vinayak.g.blr@gmail.com>
2026-05-18 21:23:20 +0530

    trivial tokenizer complete

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gpt
 repo with coding done while following Let's build GPT by Andrej Karpathy 
+https://www.youtube.com/watch?v=kCc8FmEb1nY
---
+++ gpt.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import urllib.request
+import torch
+
+input_file = Path("input.txt")
+
+if not input_file.exists():
+    try:
+        urllib.request.urlretrieve('https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt', 'input.txt')
+        print("Download complete.")
+    except Exception as e:
+        print(f"Download failed: {e}")
+
+input_text = ''
+with open('input.txt','r',encoding='utf-8') as file:
+    input_text = file.read()
+̦
+vocab =  sorted(list(set(input_text)))
+
+ctoi = {ch:i for i,ch in enumerate(vocab) }
+itoc = {i:ch for i,ch in enumerate(vocab)}
+encode = lambda s:   [ctoi(ch) for ch in s]
+decode = lambda l: ''.join([itoc(i) for i in l])
+
+
+
+
+print(vocab)
