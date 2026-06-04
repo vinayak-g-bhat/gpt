19d1180461fc84a6027ad0038a8755313c19819d
Vinayak G Bhat <vinayak.g.blr@gmail.com>
2026-06-04 06:36:04 +0530

    renamed source file

--- bigram.py
+++ bigram.py
@@ -3,6 +3,7 @@ import urllib.request
 import torch
 from torch import nn
 from torch.nn import functional as F
+import time
 
 input_file = Path("input.txt")
 
@@ -32,15 +33,32 @@ val_data = data[split_index:] #10% or left overs are kept asisde for validation
 torch.manual_seed(1337)
 block_size = 8
 batch_size = 4
+device = torch.device("cpu") if torch.backends.mps.is_available() else torch.device("cpu")
+print(device)
+eval_iters = 200
 
 def get_batch(isTraining):
     source = train_data if isTraining else val_data
     batch_offsets = torch.randint(0,len(source)-block_size,(batch_size,))
     x = torch.stack([source[batch_offset: batch_offset+block_size] for batch_offset in batch_offsets])
     y = torch.stack([source[1+batch_offset: batch_offset + block_size + 1] for batch_offset in batch_offsets]) 
+    x = x.to(device)
+    y = y.to(device)
     return x,y
 
-
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    bm.eval()
+    for split in ["train","eval"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X,Y = get_batch(split == "train")
+            logits, loss = bm(X,Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    bm.train()
+    return out
 
 
 class BigramNeuralNetwork(nn.Module):
@@ -70,17 +88,21 @@ class BigramNeuralNetwork(nn.Module):
             prompt = torch.cat((prompt,prediction),dim=1)
         return prompt
 
+start = time.time()
+
+
 xt,yt = get_batch(True)
 
 bm = BigramNeuralNetwork(len(vocab))
+bm.to(device)
 logits,loss = bm(xt,yt)
 
-prompt = torch.zeros((1,1),dtype=torch.long)
+prompt = torch.zeros((1,1),dtype=torch.long,device=device)
 
 optimizer = torch.optim.AdamW(bm.parameters())
 
 batch_size = 32
-for steps in range(10000):
+for steps in range(100000):
     xt,yt = get_batch(True)
     logtis,loss = bm(xt,yt)
     optimizer.zero_grad(set_to_none=True)
@@ -91,5 +113,8 @@ print(loss.item())
 
 
 generated_tokens = bm.generate(prompt,max_tokens=300)
+end = time.time()
+print(f"Elapsed: {end - start:.4f}s")
+
 
 print(decode(generated_tokens[0].tolist()))
